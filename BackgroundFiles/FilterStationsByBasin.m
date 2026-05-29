dfffa1660c3c1c07548670baf7652ed085f71526
Hilda <hvaras19@student.aau.dk>
2026-05-29 19:27:43 +0200

    Deleted code for second basin

--- BackgroundFiles/FilterStationsByBasin.m
+++ BackgroundFiles/FilterStationsByBasin.m
@@ -1,35 +1,20 @@
 function Stations_inside_Basin = FilterStationsByBasin( ...
-    BasinName, FirstVectorBasinName, SecondVectorBasinName, ...
-    Raw_Stations, NumberOfBasins,basinVectors)
+    BasinName, Raw_Stations, basinVectors)
 
     % Find and load polygon
     poly1 = load(sprintf('%s\\%s.vec', basinVectors,BasinName));
 
-    % If there are two polygons
-    if NumberOfBasins == 2
-        poly2 = load(sprintf('%s\\%s.vec', basinVectors,BasinName));
-    end
-
-    % Extract the data variable
+    % Add all stations in struct
     Stations_inside_Basin = Raw_Stations;
 
-    % Remove stations outside the polygon(s)
+    % Finds stations that are inside the polygon
     for i = length(Stations_inside_Basin):-1:1
-        insideBasin1 = inpolygon( ...
+        insideBasin = inpolygon( ...
             Stations_inside_Basin(i).Lon, ...
             Stations_inside_Basin(i).Lat, ...
             poly1(:,1), poly1(:,2));
-
-        if NumberOfBasins == 2
-            insideBasin2 = inpolygon( ...
-                Stations_inside_Basin(i).Lon, ...
-                Stations_inside_Basin(i).Lat, ...
-                poly2(:,1), poly2(:,2));
-        else
-            insideBasin2 = false;
-        end
-
-        if ~(insideBasin1 || insideBasin2)
+        % Removes stations outside the polygon
+        if ~insideBasin
             Stations_inside_Basin(i) = [];
         end
     end

1c45dfb29655e2a3288a815e8f76335ce6c3d93a
Hilda <hvaras19@student.aau.dk>
2026-05-29 20:05:38 +0200

    Deleted code of second basin

--- Files2Download_CLMS_Data/CLMS_Extract_Stations_from_CSV.m
+++ Files2Download_CLMS_Data/CLMS_Extract_Stations_from_CSV.m
@@ -10,36 +10,13 @@ basinVectors = '..\BackgroundFiles\33_Main_world_basin_Vectors'; % shapefile con
 toc
 
 %% --- Change input in this section
-% Name of the basin(s) working with
+% Name of the basin working with
 BasinName = 'Niger';
-
-% Amount of basins
-numBasins = 1; % if 1 is the input, then Basin2 will not be shown
-
-% Name of first basin
-Basin1 = 'Niger'; % 'Danube' or 'Ganges'
-
-BasinColor1 = 'y'; % Color of basin contour
-
-% Name of second Basin
-Basin2 = 'Brahmaputra'; % 'Brahmaputra'
-
-BasinColor2 = 'w'; % Color of basin contour
-
-% Line width for the basins
-BasinLineWidth = 2;
-
-if numBasins == 2 
-    CombineBasin = sprintf('%s-%s', Basin1, Basin2);
-else
-    CombineBasin = Basin1;
-end
 
 %% Select those stations that are inside basin
 tic
 Stations_inside_Basin = FilterStationsByBasin( ...
-    BasinName, Basin1, Basin2, ...
-    SavedData, numBasins,basinVectors);
+    BasinName, SavedData, basinVectors);
 toc
 
 %% Saving as CSV
